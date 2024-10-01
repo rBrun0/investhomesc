acfdfb630bda282da6d4325e7f8282f7cf009aeb
rBrun0 <romerobruno788@gmail.com>
2024-10-01 15:24:30 -0300

    melhora nos tipos - dezessete

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions/v1';
+import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import cors from 'cors';
 const corsHandler = cors({ origin: true });
