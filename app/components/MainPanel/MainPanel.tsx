c55a2983d4286d62ba375b92070bcd59e4879dc1
rBrun0 <romerobruno788@gmail.com>
2024-10-04 11:15:53 -0300

    imagem de praia adicionada

--- app/components/MainPanel/MainPanel.tsx
+++ app/components/MainPanel/MainPanel.tsx
@@ -3,6 +3,7 @@ import beachImage from '../../assets/mainBeach.jpg'
 import { RoomsList } from "./components/RoomsList/RoomsList"
 import { SelectCamp } from "./components/SelectCamp/SelectCamp"
 import Link from "next/link"
+import praia from "@/app/assets/praia.jpg"
 import { MinAndMaxValues } from "./components/MinAndMaxValues"
 
 export const MainPanel = () => {
@@ -12,13 +13,15 @@ export const MainPanel = () => {
     return (
 
     <main className="w-full relative h-[600px] md:h-[460px] flex justify-center items-center text-white">
-        <Image src={beachImage} alt="imagem-praia" fill objectFit="cover" className="absolute -z-50"/>
+        <Image src={praia} alt="imagem-praia" fill objectFit="cover" className="absolute -z-50"/>
 
         <section className="w-11/12 h-[34rem] md:h-[24rem] lg:h-96 rounded-md flex flex-col justify-start items-center space-y-6 relative">
 
             <div className="absolute left-0 top-0 bg-black w-full h-full -z-10 opacity-60"/>
 
             <h1 className="text-white text-center text-2xl md:text-3xl tracking-wider lg:text-4xl px-3 pt-6 font-semibold">Somente Vendas! Não trabalhamos com aluguel</h1>
+
+
 
             <div className="flex flex-wrap justify-center w-[90%] gap-2 md:gap-4 lg:gap-8  ">
 
