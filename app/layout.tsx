140bc52f9e15459f8bb7bd9fd82c8786f3543faa
rBrun0 <romerobruno788@gmail.com>
2025-02-19 20:27:35 -0300

    fixing errors 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
       <body
