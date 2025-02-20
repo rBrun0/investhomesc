afc43a19d07ce2029d4092d2ea3dd91a4ba3aafe
rBrun0 <romerobruno788@gmail.com>
2025-02-19 21:45:48 -0300

    fixing errors 8

--- app/components/Header/components/Entre.tsx
+++ app/components/Header/components/Entre.tsx
@@ -1,32 +1,32 @@
 'use client'
 
-import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut, User } from "firebase/auth"
+import { signOut, User } from "firebase/auth"
 import { auth } from "@/app/firebaseConfig"
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import {  useState } from "react"
+// import { useDispatch } from "react-redux"
 // import { clearUser, setUser } from "@/app/features/user/userSlices"
-import { RootState } from "@/app/store"
-import { clearUser, setUser } from "@/app/features/user/userSlices"
+// import { RootState } from "@/app/store"
+// import { clearUser, setUser } from "@/app/features/user/userSlices"
 import Link from "next/link"
 
 export const Entre = () => {
-    const dispatch = useDispatch()
+    // const dispatch = useDispatch()
 
-    const [usuario, setUsuario] = useState<User>()
-    const [actualUser, setActualUser] = useState<User | null>(null)
-    const usuarioCorrente = auth.currentUser
+    // const [usuario] = useState<User>()
+    const [actualUser] = useState<User | null>(null)
+    // const usuarioCorrente = auth.currentUser
 
-    const provider = new GoogleAuthProvider();
+    // const provider = new GoogleAuthProvider();
 
-    function handleSignIn() {
-        signInWithPopup(auth, provider).then((result) => {
-            console.log("User signed in successfully!", result);
-            console.log(result.user)
-            setUsuario(result.user)
-        }).catch(() => {
-            console.log("Error signing in with popup");
-        })
-    }
+    // function handleSignIn() {
+    //     signInWithPopup(auth, provider).then((result) => {
+    //         console.log("User signed in successfully!", result);
+    //         console.log(result.user)
+    //         setUsuario(result.user)
+    //     }).catch(() => {
+    //         console.log("Error signing in with popup");
+    //     })
+    // }
 
     const logout = async () => {
         try {
@@ -37,24 +37,27 @@ export const Entre = () => {
         }
     };
 
-const user = useSelector((state: RootState) => state.userSlice);  // Obtém o estado do usuário
+// const user = useSelector((state: RootState) => state.userSlice);  // Obtém o estado do usuário
 
 
-useEffect(() => {
-   if(usuario) {
-    dispatch(setUser(usuario))
-   }
+// useEffect(() => {
+//    if(usuario) {
+//     dispatch(setUser(usuario))
+//    }
 
-   const unsubscribe = onAuthStateChanged(auth, (actUser) => {
-    setActualUser(actUser);
-  });
+//    const unsubscribe = onAuthStateChanged(auth, (actUser) => {
+//     setActualUser(actUser);
+//   });
 
-  console.log('unsubscribe', actualUser)
-  console.log('usuario corrente', usuarioCorrente)
+//   console.log('actualUser', actualUser)
+//   console.log('usuario', usuario)
 
-  return () => unsubscribe();
+//   console.log('unsubscribe', actualUser)
+//   console.log('usuario corrente', usuarioCorrente)
 
-}, [])
+//   return () => unsubscribe();
+
+// }, [])
 
     return (<>
     {
