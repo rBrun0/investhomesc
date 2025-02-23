34e812dcaddd80005656f28b0ed3363ec8ff3b3b
rBrun0 <romerobruno788@gmail.com>
2025-02-22 23:37:25 -0300

    fixing errors 20

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,13 +1,16 @@
-import * as admin from "firebase-admin";
+import admin from "firebase-admin";
 
+// Verifica se o admin já foi inicializado
 if (!admin.apps.length) {
+  const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Corrige a quebra de linha
+  };
+
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
-export {admin};
+export { admin };
